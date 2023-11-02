1fc1c7193e0722ab038cff0da4df2807c982aaed
Victor-Lis <victorlisbronzo1@gmail.com>
2023-11-02 14:38:42 -0300

    Revert "v1"
    This reverts commit b5911f6f86bceb3929c06f4a6d60474bde0da8b6.

--- src/Layout/WelcomeBanner/index.js
+++ src/Layout/WelcomeBanner/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Container, Mascote, Message, Arrow } from './styles'
-import MascoteSRC from '../../Assets/Mascote.png'
+import { Container, Message, Arrow } from './styles'
 
 export default function WelcomeBanner({navigate}) {
 
   return (
     <Container>
         <Message> Bem vindos <br/> futuros Etequianos!! </Message>
-        <Mascote src={MascoteSRC}/>
         <Arrow onClick={() => navigate()}/>
     </Container>
   )
