2a0871f5348a901b945d22951db834d1597c2bad
Victor Lis Bronzo <109773129+Victor-Lis@users.noreply.github.com>
2023-11-07 23:55:53 -0300

    Atualizar o index.js

--- src/Layout/Choose/index.js
+++ src/Layout/Choose/index.js
@@ -9,12 +9,13 @@ export default function Choose({navigate}) {
         <Title> Conheça os cursos </Title>
           <SubTitle> Administração </SubTitle>
           <VideoIframe src={"https://youtube.com/embed/tIWWroRfxOM"}/>
-
-          <SubTitle> Marketing </SubTitle>
-          <VideoIframe/>
           
           <SubTitle> Desenvolvimento de Sistemas </SubTitle>
-          <VideoIframe/>
+          <VideoIframe src={"https://youtube.com/embed/d9i2Jp0f0Yk"}/>
+
+          <SubTitle> Marketing </SubTitle>
+          <VideoIframe src={"https://youtube.com/embed/Mz6_VoDy1dw"}/>
+
     </Container>
   )
 }
